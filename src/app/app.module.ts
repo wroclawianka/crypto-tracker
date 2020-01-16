abd56c35cb5a7d530ed7e0d5cf29f692ca31eccb
Dorota Zelga <dorota.zelga@gmail.com>
2020-01-16 21:14:38 +0100

    Routes created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 import {CoinApiService} from "./services/coin-api.service";
 
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
-import { SearchComponent} from "./components/search/search.component";
+import {SearchComponent} from "./components/home-page/search/search.component";
 
-import { MatSliderModule } from '@angular/material/slider';
+import {MatSliderModule} from '@angular/material/slider';
 import {_MatMenuDirectivesModule, MatMenuModule} from "@angular/material/menu";
 import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 
+import {RouterModule, Routes} from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {EditFavouritesComponent} from './components/edit-favourites/edit-favourites.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'edit-favourites',
+    component: EditFavouritesComponent
+  },
+  {
+    path: '',
+    component: HomePageComponent,
+    pathMatch: 'full'
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
+    HomePageComponent,
+    EditFavouritesComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,10 +54,14 @@ import {MatInputModule} from "@angular/material/input";
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [
     CoinApiService
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
