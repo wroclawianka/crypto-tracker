64f012475cb5fabee730bb9dbcaac836d3e2070f
Dorota Zelga <dorota.zelga@gmail.com>
2020-01-16 21:04:44 +0100

    Coin API Service crated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
+import {CoinApiService} from "./services/coin-api.service";
+
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatSliderModule} from '@angular/material/slider';
@@ -32,7 +34,9 @@ import {MatInputModule} from "@angular/material/input";
     MatFormFieldModule,
     MatInputModule,
   ],
-  providers: [],
+  providers: [
+    CoinApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
