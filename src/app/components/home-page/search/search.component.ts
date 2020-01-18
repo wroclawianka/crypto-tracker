46cdbded13f67fe6b601a0fa8e482401921ff560
Dorota Zelga <dorota.zelga@gmail.com>
2020-01-18 21:56:51 +0100

    Search component created

--- src/app/components/home-page/search/search.component.ts
+++ src/app/components/home-page/search/search.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Asset} from "../../../models/asset.model";
+import {Observable} from "rxjs";
+import * as fromStore from '../../../store'
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  searchValue: string;
+  assets$: Observable<Array<Asset>>;
 
-  constructor() { }
+  constructor(private store: Store<fromStore.AssetsState>) {
+    this.searchValue = "";
+  }
 
   ngOnInit() {
+    this.store.select<any>('assets').subscribe(state => {
+      this.assets$ = state.assets;
+    });
+    this.store.dispatch(new fromStore.FetchAssets())
+  }
+
+  onKey(e) {
+    this.searchValue = e.target.value;
+  }
+
+  searchAssets() {
+    debugger;
+    // this.store.dispatch(Search(this.searchValue))
   }
 
 }
